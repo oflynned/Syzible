048c3a191abc43e4c1301630ed3fc013df2eab34
Ed <oflynned@tcd.ie>
2016-10-29 19:28:04 +0100

    filtering all working so far for whole page

--- routes/Tearma/Scrape.js
+++ routes/Tearma/Scrape.js
@@ -19,9 +19,13 @@ var genderMap = {
 };
 
 var typeMap = {
-    s: 'noun',
-    br: 'verb',
-    a: 'adjective'
+    "s": 'noun',
+    "br": 'verb',
+    "a": 'adjective',
+    "réimír": 'prefix',
+    "v": "verb",
+    "phr.": "phrase",
+    "frása" : "phrase"
 };
 
 router.get('/', function (req, res) {
@@ -39,7 +43,10 @@ router.get('/', function (req, res) {
     // if verb
     var gerund, participle, preposition;
 
+    var searchDeclension = 0, searchGender= "";
+
     var term = req.query['term'];
+    term = decodeURIComponent(term);
     var lang;
     if (req.query['lang'] != undefined)
         lang = req.query['lang'] === "en" ? EN_PARAM : GA_PARAM;
@@ -50,19 +57,80 @@ router.get('/', function (req, res) {
         var $ = cheerio.load(html);
 
         //first dArticle is definitions, subsequent are related terms
-        $('.dArticle').first().filter(function () {
-            searchType = $(this).find('.dHeadTerm > .dTerm > .dTermHeadline > span > a > > span > span').text();
-            $(this).find('.dHeadTerm > .dTerm > .dTermHeadline > span > a > span > span > .dAnnotPOS').empty();
-            searchTerm = $(this).find('.dHeadTerm > .dTerm > .dTermHeadline > span > a > span > span').text().trim();
+        $('.dArticle').filter(function () {
+            var searchMutations = [];
+
+            $(this).find('.dHeadTerm > .dTerm > .dTermHeadline > span > a > span').each(function () {
+                searchType = $(this).find('.dAnnotPOS').text();
+                searchDeclension = parseInt(searchType.match(/\d+/));
+                searchGender = searchType.replace(/[0-9]/g, '');
+
+                $(this).find('.dAnnotPOS').empty();
+                searchTerm = $(this).text().trim();
+                searchMutations.push({root: searchTerm});
+            });
+
+            $(this).find('.dHeadTerm > .dTerm > .dTermSubline > .dInflect').each(function () {
+                var label = $(this).find('.dLabel').text().replace(/[^a-zA-Z]+/g, '');
+                var mutation = $(this).find('.dValue').text();
+
+                // nouns
+                if (label === "gu")
+                    searchMutations.push({genSing: mutation});
+                else if (label === "gi")
+                    searchMutations.push({genPlu: mutation});
+                else if (label === "ai")
+                    searchMutations.push({nomPlu: mutation});
+                else if (label === "iol") {
+                    searchMutations.push({genPlu: mutation});
+                    searchMutations.push({nomPlu: mutation});
+                }
+
+                // verbs
+                else if (label === "abr")
+                    searchMutations.push({gerund: mutation});
+                else if (label === "aidbhr")
+                    searchMutations.push({participle: mutation});
+            });
 
             //determine details about the word from each bullet point
             $(this).find('.dSense').each(function () {
                 var domains = [];
-                $(this).find('.dDomains > .dDomain > span').each(function () {
-                    if ($(this).is('.dIrish'))
-                        domains.push({domain_ga: $(this).text()});
-                    else if ($(this).is('.dEnglish'))
-                        domains.push({domain_en: $(this).text()});
+                $(this).find('.dDomains > .dDomain').each(function () {
+                    domains.push({
+                        ga: $(this).find('.dIrish').text(),
+                        en: $(this).find('.dEnglish').text()
+                    });
+                });
+
+                var synonyms = [];
+                // synonyms area below the search word
+                $(this).find('.dSynonyms > .dTerm').each(function () {
+                    $(this).find('span > a > span > span').each(function () {
+                        var metaData = $(this).find('.dAnnotPOS').text();
+                        var type = metaData.replace(/[0-9]/g, '');
+                        var syn_declension = parseInt(metaData.match(/\d+/g, ''));
+
+                        $(this).find('.dAnnotPOS').empty();
+                        var term = $(this).parent().text().trim();
+
+                        synonyms.push({
+                            synonym: term,
+                            type: typeMap[type],
+                            declension: syn_declension
+                        });
+                    });
+                });
+
+                var examples = [];
+                $(this).find('.dExamples > .dExample').each(function () {
+                    var source = $(this).find('.dSource').text();
+                    var target = $(this).find('.dTarget').text();
+
+                    examples.push({
+                        source: source,
+                        target: target
+                    });
                 });
 
                 $(this).find('.dTargetTerms > .dTerm').each(function () {
@@ -70,11 +138,11 @@ router.get('/', function (req, res) {
 
                     $(this).find('.dTermHeadline > span > a > .dWording > .dStretch').each(function () {
                         metaData = $(this).find('.dAnnotPOS').text();
-                        declension = metaData.match(/\d+/)[0];
+                        declension = parseInt(metaData.match(/\d+/));
                         gender = metaData.replace(/[0-9]/g, '');
 
                         $(this).find('.dAnnotPOS').empty();
-                        mutations.push({root: $(this).text().trim()});
+                        mutations.push({root: $(this).parent().text().trim().replace("  ", " ")});
                     });
 
                     $(this).find('.dTermSubline > .dInflect').each(function () {
@@ -94,16 +162,23 @@ router.get('/', function (req, res) {
                         }
 
                         //verbs
+                        else if (label === "abr")
+                            mutations.push({gerund: mutation});
+                        else if (label === "aidbhr")
+                            mutations.push({participle: mutation});
                     });
 
                     data.push({
                         searchTerm: searchTerm,
                         searchType: typeMap[searchType],
-                        root: root,
+                        searchDeclension: searchDeclension,
+                        searchGender: genderMap[searchGender],
+                        searchMutations: searchMutations,
+                        synonyms: synonyms,
                         gender: genderMap[gender],
-                        declension: declension,
                         mutations: mutations,
-                        domains: domains
+                        domains: domains,
+                        examples: examples
                     });
                 });
 
