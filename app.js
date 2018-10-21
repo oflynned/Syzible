25845383f4824c6ecae3b50e992478c3262dc3b6
Edmond O' Flynn <oflynned@tcd.ie>
2018-10-21 18:08:14 +0100

    remove unused routes and apk files

--- app.js
+++ app.js
@@ -41,37 +41,6 @@ app.use(function (req, res, next) {
     next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function (err, req, res) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
-//prevent heroku app from sleeping through pinging
-if (process.env["NO_SLEEPING"] === 1) {
-    setInterval(function () {
-        http.get("http://www.syzible.com");
-    }, 900000);
-}
-
 hbs.registerHelper('compare', function (lvalue, operator, rvalue, options) {
     var operators, result;
 
