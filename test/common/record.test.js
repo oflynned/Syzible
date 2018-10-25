a6b0fc5fd19967cdda9c035a124f9d4ef5327272
Edmond O' Flynn <oflynned@tcd.ie>
2018-10-25 23:33:01 +0100

    added more tests

--- test/common/record.test.js
+++ test/common/record.test.js
@@ -1,10 +1,19 @@
 let record = require("../../common/record");
+let chai = require('chai'), expect = chai.expect;
 
-let chai = require('chai'), expect = chai.expect, should = chai.should();
-let chaiAsPromised = require("chai-as-promised");
-chai.use(chaiAsPromised);
+const config = require('../../config/db');
+const db = require('monk')(config.mongoUrl);
+const collection = "collection";
 
 describe("db operations", () => {
+    beforeEach((done) => {
+        dropDb().then(() => done());
+    });
+
+    function dropDb() {
+        return db.get(collection).drop();
+    }
+
     describe("create", () => {
         [undefined, null, ""].forEach((fixture) => {
             it(`should not allow ${fixture} as a collection name`, (done) => {
@@ -19,7 +28,7 @@ describe("db operations", () => {
 
         [undefined, null, ""].forEach((fixture) => {
             it(`should not allow ${fixture} as data`, (done) => {
-                record.createRecord("collection_name", fixture)
+                record.createRecord(collection, fixture)
                     .then(() => done("should have failed"))
                     .catch((err) => {
                         expect(err.message).to.equal("empty_data");
@@ -29,7 +38,7 @@ describe("db operations", () => {
         });
 
         it('should create a new record', (done) => {
-            record.createRecord("collection_name", {test: 123})
+            record.createRecord(collection, {test: 123})
                 .then((data) => {
                     expect(data).to.be.an("object");
                     expect(data).to.have.property("_id");
@@ -38,6 +47,81 @@ describe("db operations", () => {
                     done()
                 })
                 .catch((err) => done(err));
+        });
+    });
+
+    describe("get", () => {
+        beforeEach((done) => {
+            record.createRecord(collection, {test: 123})
+                .then(() => done())
+        });
+
+        it('should retrieve with filter', (done) => {
+            record.getRecords(collection, {test: 123})
+                .then((data) => {
+                    expect(data).to.be.an("array");
+                    expect(data.length).to.equal(1);
+                    expect(data[0]).to.have.property("_id");
+                    expect(data[0]).to.have.property("test");
+                    done();
+                })
+                .catch((err) => done(err))
+        });
+
+
+        it('should retrieve without filter', (done) => {
+            record.getRecords(collection, {})
+                .then((data) => {
+                    expect(data).to.be.an("array");
+                    expect(data.length).to.equal(1);
+                    expect(data[0]).to.have.property("_id");
+                    expect(data[0]).to.have.property("test");
+                    done();
+                })
+                .catch((err) => done(err))
+        });
+    });
+
+    describe("modify", () => {
+        let data;
+
+        beforeEach((done) => {
+            record.createRecord(collection, {test: 123})
+                .then((_data) => data = _data)
+                .then(() => done())
+        });
+
+        it('should modify record', (done) => {
+            let modified = Object.assign({}, data, {test: 321});
+            record.modifyRecord(collection, modified, modified["_id"])
+                .then((data) => {
+                    expect(data).to.be.an("object");
+                    expect(data).to.have.property("_id");
+                    expect(data).to.have.property("test");
+                    expect(data.test).to.equal(321);
+                    done();
+                })
+                .catch((err) => done(err))
+        });
+    });
+
+    describe("delete", () => {
+        let id;
+
+        beforeEach((done) => {
+            record.createRecord(collection, {test: 123})
+                .then(({_id}) => id = _id)
+                .then(() => done())
+        });
+
+        it('should delete record', (done) => {
+            record.deleteRecord(collection, id)
+                .then(() => record.getRecords(collection, {}))
+                .then((data) => {
+                    expect(data.length).to.equal(0);
+                    done();
+                })
+                .catch((err) => done(err))
         });
     });
 });
