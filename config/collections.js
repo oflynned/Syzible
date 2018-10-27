00a695d3bd991a65fae13b24eb055256da984241
Edmond O' Flynn <oflynned@tcd.ie>
2018-10-27 18:19:23 +0100

    added collection and environment configs

--- config/collections.js
+++ config/collections.js
@@ -1,5 +1,11 @@
-module.exports = {
-    tearma: [
-        "nouns", "verbs", "adjectives", "phrases", "abbreviations"
-    ]
-};
+function classifyEnvironment(env) {
+    let dbName = env === "production" ? "tearma" : `tearma_${env}`;
+    return {
+        "dbName": dbName,
+        "collections": [
+            "nouns", "verbs", "adjectives", "phrases", "abbreviations"
+        ]
+    };
+}
+
+module.exports = (env) => classifyEnvironment(env);
