e85ec13d3366e8745cc0c11be0e1d6ae26ce9fbc
Edmond O' Flynn <oflynned@tcd.ie>
2018-10-29 16:06:08 +0000

    updated routes and controller for fetching tbx file and filename

--- routes/tearma/v2/index.js
+++ routes/tearma/v2/index.js
@@ -1,8 +1,14 @@
 let express = require('express');
 let router = express.Router();
 
+const datastoreController = require("./controllers/datastoreController");
+
 module.exports = ({dbName}) => {
-    router.get('/', (req, res) => res.json({db: dbName}));
+    router.get('/', (req, res) => {
+        return datastoreController.fetchTbxFile()
+            .then((data) => res.send(""))
+            .catch((err) => res.send(err))
+    });
 
     return router;
 };
