63448dc1036f12d4fda73841da1ed9b376b903b2
Edmond O' Flynn <oflynned@tcd.ie>
2018-11-17 16:08:41 +0000

    updated test fixtures for noun parser

--- test/routes/tearma/v2/helpers/nounParser.test.js
+++ test/routes/tearma/v2/helpers/nounParser.test.js
@@ -20,106 +20,88 @@ describe("noun parsing", () => {
 			return Promise.all([db.get("nouns").drop()]);
 		}
 
+		function loadFixture (fixture, length) {
+			return new Promise((resolve) => {
+				nounParser.parseNounsFromData(fixture)
+					.then(() => nounModel.findAll())
+					.then((nouns) => {
+						expect(nouns).to.be.an("array");
+						expect(nouns.length).to.equal(length);
+						resolve(nouns);
+					})
+			});
+		}
+
 		it("with separate plurals", (done) => {
-			nounParser.parseNounsFromData(fixtures.separatePlurals)
-				.then(() => nounModel.findAll())
+			loadFixture(fixtures.separatePlurals, 1)
 				.then((nouns) => {
-					expect(nouns).to.be.an("array");
-					expect(nouns.length).to.equal(1);
-					return nouns[0];
-				})
-				.then((noun) => {
-					expect(noun).to.be.an("object");
-					expect(noun.en.term).to.equal("adductor muscle");
-					expect(noun.ga.term).to.equal("matán aduchtach");
-					expect(noun.ga.mutations.nominativeSingular).to.equal("matán aduchtach");
-					expect(noun.ga.mutations.genitiveSingular).to.equal("matáin aduchtaigh");
-					expect(noun.ga.mutations.nominativePlural).to.equal("matáin aduchtacha");
-					expect(noun.ga.mutations.genitivePlural).to.equal("matán aduchtach");
-					expect(noun.ga.declension).to.equal(1);
-					expect(noun.ga.gender).to.equal("masculine");
+					expect(nouns[0]).to.be.an("object");
+					expect(nouns[0].en.term).to.equal("adductor muscle");
+					expect(nouns[0].ga.term).to.equal("matán aduchtach");
+					expect(nouns[0].ga.mutations.nominativeSingular).to.equal("matán aduchtach");
+					expect(nouns[0].ga.mutations.genitiveSingular).to.equal("matáin aduchtaigh");
+					expect(nouns[0].ga.mutations.nominativePlural).to.equal("matáin aduchtacha");
+					expect(nouns[0].ga.mutations.genitivePlural).to.equal("matán aduchtach");
+					expect(nouns[0].ga.declension).to.equal(1);
+					expect(nouns[0].ga.gender).to.equal("masculine");
 					done();
 				})
 				.catch((err) => done(err));
 		});
 
 		it("with combined plurals", (done) => {
-			nounParser.parseNounsFromData(fixtures.combinedPlural)
-				.then(() => nounModel.findAll())
+			loadFixture(fixtures.combinedPlural, 1)
 				.then((nouns) => {
-					expect(nouns).to.be.an("array");
-					expect(nouns.length).to.equal(1);
-					return nouns[0];
-				})
-				.then((noun) => {
-					expect(noun).to.be.an("object");
-					expect(noun.en.term).to.equal("occupier");
-					expect(noun.ga.term).to.equal("áititheoir");
-					expect(noun.ga.mutations.nominativeSingular).to.equal("áititheoir");
-					expect(noun.ga.mutations.genitiveSingular).to.equal("áititheora");
-					expect(noun.ga.mutations.nominativePlural).to.equal("áititheoirí");
-					expect(noun.ga.mutations.genitivePlural).to.equal("áititheoirí");
-					expect(noun.ga.declension).to.equal(3);
-					expect(noun.ga.gender).to.equal("masculine");
+					expect(nouns[0]).to.be.an("object");
+					expect(nouns[0].en.term).to.equal("occupier");
+					expect(nouns[0].ga.term).to.equal("áititheoir");
+					expect(nouns[0].ga.mutations.nominativeSingular).to.equal("áititheoir");
+					expect(nouns[0].ga.mutations.genitiveSingular).to.equal("áititheora");
+					expect(nouns[0].ga.mutations.nominativePlural).to.equal("áititheoirí");
+					expect(nouns[0].ga.mutations.genitivePlural).to.equal("áititheoirí");
+					expect(nouns[0].ga.declension).to.equal(3);
+					expect(nouns[0].ga.gender).to.equal("masculine");
 					done();
 				})
 				.catch((err) => done(err));
 		});
 
 		it("with only nominative singular", (done) => {
-			nounParser.parseNounsFromData(fixtures.onlyNominativeSingularForm)
-				.then(() => nounModel.findAll())
+			loadFixture(fixtures.onlyNominativeSingularForm, 1)
 				.then((nouns) => {
-					expect(nouns).to.be.an("array");
-					expect(nouns.length).to.equal(1);
-					return nouns[0];
-				})
-				.then((noun) => {
-					expect(noun).to.be.an("object");
-					expect(noun.en.term).to.equal("contingent charge");
-					expect(noun.ga.term).to.equal("muirear teagmhasach");
-					expect(noun.ga.mutations.nominativeSingular).to.equal("muirear teagmhasach");
-					expect(noun.ga.mutations.genitiveSingular).to.equal(null);
-					expect(noun.ga.mutations.nominativePlural).to.equal(null);
-					expect(noun.ga.mutations.genitivePlural).to.equal(null);
-					expect(noun.ga.declension).to.equal(1);
-					expect(noun.ga.gender).to.equal("masculine");
+					expect(nouns[0]).to.be.an("object");
+					expect(nouns[0].en.term).to.equal("contingent charge");
+					expect(nouns[0].ga.term).to.equal("muirear teagmhasach");
+					expect(nouns[0].ga.mutations.nominativeSingular).to.equal("muirear teagmhasach");
+					expect(nouns[0].ga.mutations.genitiveSingular).to.equal(null);
+					expect(nouns[0].ga.mutations.nominativePlural).to.equal(null);
+					expect(nouns[0].ga.mutations.genitivePlural).to.equal(null);
+					expect(nouns[0].ga.declension).to.equal(1);
+					expect(nouns[0].ga.gender).to.equal("masculine");
 					done();
 				})
 				.catch((err) => done(err));
 		});
 
 		it("with no declension", (done) => {
-			nounParser.parseNounsFromData(fixtures.noDeclension)
-				.then(() => nounModel.findAll())
+			loadFixture(fixtures.noDeclension, 1)
 				.then((nouns) => {
-					expect(nouns).to.be.an("array");
-					expect(nouns.length).to.equal(1);
-					return nouns[0];
-				})
-				.then((noun) => {
-					expect(noun).to.be.an("object");
-					expect(noun.en.term).to.equal("customs");
-					expect(noun.ga.term).to.equal("custaim");
-					expect(noun.ga.mutations.nominativeSingular).to.equal("custaim");
-					expect(noun.ga.mutations.genitiveSingular).to.equal(null);
-					expect(noun.ga.mutations.nominativePlural).to.equal(null);
-					expect(noun.ga.mutations.genitivePlural).to.equal("custam");
-					expect(noun.ga.declension).to.equal(-1);
-					expect(noun.ga.gender).to.equal("masculine");
+					expect(nouns[0]).to.be.an("object");
+					expect(nouns[0].en.term).to.equal("customs");
+					expect(nouns[0].ga.term).to.equal("custaim");
+					expect(nouns[0].ga.mutations.nominativeSingular).to.equal("custaim");
+					expect(nouns[0].ga.mutations.genitiveSingular).to.equal(null);
+					expect(nouns[0].ga.mutations.nominativePlural).to.equal(null);
+					expect(nouns[0].ga.mutations.genitivePlural).to.equal("custam");
+					expect(nouns[0].ga.declension).to.equal(-1);
+					expect(nouns[0].ga.gender).to.equal("masculine");
 					done();
 				})
 				.catch((err) => done(err));
 		});
 
 		it("with multiple English terms to one Irish term", (done) => {
-			nounParser.parseNounsFromData(fixtures.multipleEnglishToOneIrish)
-				.then(() => nounModel.findAll())
-				.then((nouns) => {
-					expect(nouns).to.be.an("array");
-					expect(nouns.length).to.equal(2);
-					return nouns;
-				})
+			loadFixture(fixtures.multipleEnglishToOneIrish, 2)
 				.then((nouns) => {
 					expect(nouns[0]).to.be.an("object");
 					expect(nouns[0].en.term).to.equal("heir-at-law");
@@ -147,13 +129,7 @@ describe("noun parsing", () => {
 		});
 
 		it("with multiple Irish terms to one English term", (done) => {
-			nounParser.parseNounsFromData(fixtures.multipleIrishToOneEnglish)
-				.then(() => nounModel.findAll())
-				.then((nouns) => {
-					expect(nouns).to.be.an("array");
-					expect(nouns.length).to.equal(2);
-					return nouns;
-				})
+			loadFixture(fixtures.multipleIrishToOneEnglish, 2)
 				.then((nouns) => {
 					expect(nouns[0]).to.be.an("object");
 					expect(nouns[0].en.term).to.equal("customs clearance");
