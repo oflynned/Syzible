558c9c60e6adf6cd17b4c6bbd49e429391038b11
Edmond O' Flynn <oflynned@tcd.ie>
2018-11-25 17:32:40 +0000

    added styling to card components and centering of items

--- ui/src/components/termCard.jsx
+++ ui/src/components/termCard.jsx
@@ -12,6 +12,10 @@ export default class TermCard extends Component {
         this.state = props
     }
 
+    style = {
+        height: 48,
+    };
+
     mutate = (mutation, func, count) => {
         if(func === "nominative" && count === "singular") {
             return "an " + mutation.nominativeSingular;
@@ -47,38 +51,41 @@ export default class TermCard extends Component {
         const { ga, en } = this.state.result;
         return  <Card className="card">
                     <div className="contents">
-                        <Typography variant="h5">{this.capitalise(ga.term)} / {this.capitalise(en.term)}</Typography>
                         <div>
+                            <Typography variant="h5" className="header">{this.capitalise(ga.term)} / {this.capitalise(en.term)}</Typography>
+                        </div>
+                        <div className="margin-content">
                             <Chip label="noun" className="chip"/>
                             <Chip label={ga.gender} className="chip"/>
                             <Chip label={this.ordinate(ga.declension)} className="chip"/>
                             <Chip label="politics" className="chip"/>
                             <Chip label="history" className="chip"/>
                         </div>
-                        <div>
+                        <div className="margin-content">                        
                             <Grid container spacing={16}>
                                 <Grid item xs={6}>
                                     <Typography variant="h5">{this.mutate(ga.mutations, "nominative", "singular")}</Typography>
-                                    <Typography variant="subtitle1">Nominative Singular</Typography>
+                                    <Typography variant="subtitle1" color="textSecondary">Nominative Singular</Typography>
                                 </Grid>
-
                                 <Grid item xs={6}>
                                     <Typography variant="h5">{this.mutate(ga.mutations, "nominative", "plural")}</Typography>
-                                    <Typography variant="subtitle1">Nominative Plural</Typography>
+                                    <Typography variant="subtitle1" color="textSecondary">Nominative Plural</Typography>
                                 </Grid>
                             </Grid>
                             <Grid container spacing={16}>
                                 <Grid item xs={6}>                                    
                                     <Typography variant="h5">{this.mutate(ga.mutations, "genitive", "singular")}</Typography>
-                                    <Typography variant="subtitle1">Genitive Singular</Typography>
+                                    <Typography variant="subtitle1" color="textSecondary">Genitive Singular</Typography>
                                 </Grid>
                                 <Grid item xs={6}>
                                     <Typography variant="h5">{this.mutate(ga.mutations, "genitive", "plural")}</Typography>
-                                    <Typography variant="subtitle1">Genitive Plural</Typography>
+                                    <Typography variant="subtitle1" color="textSecondary">Genitive Plural</Typography>
                                 </Grid>
                             </Grid>
                         </div>
-                        <Typography>Related terms: administration</Typography>
+                        <div className="margin-content">
+                            <Typography color="textSecondary">Related terms: administration</Typography>
+                        </div>
                     </div>
                 </Card>
     }
--- ui/src/components/termCardList.jsx
+++ ui/src/components/termCardList.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-
 import TermCard from "./termCard";
+
+import "./termCardList.css"
 
 export default class TermCardList extends Component {
     constructor(props) {
         super(props);
         this.state = { results: props["results"] }
     }
-    
+
     render() {
         const { results } = this.state;
         return ( 
