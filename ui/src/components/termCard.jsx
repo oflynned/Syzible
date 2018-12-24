ea2d99679d8948a7ee2ebff3d55cd5a852814173
Edmond O' Flynn <oflynned@tcd.ie>
2018-12-24 00:18:29 +0000

    added logic to term card to lenite and eclipse singular words

--- ui/src/components/termCard.jsx
+++ ui/src/components/termCard.jsx
@@ -11,23 +11,72 @@ export default class TermCard extends Component {
         height: 48,
     };
 
+    mutate = (noun, gender, func, count) => {
+        if(noun === null) return noun;
+        
+        if(noun.split(" ").length !== 1) return noun;
+
+        if(func === "nominative" && count === "singular" && gender === "feminine") {
+            return this.lenite(noun);
+        } 
+        
+        if(func === "genitive") {
+            if(count === "singular" && gender === "masculine")
+                return this.lenite(noun);
+            else if(count === "plural")
+                return this.eclipse(noun)
+        }
+
+        return noun;
+    }
+
     lenite = (noun) => {
+        const lenitableInitials = {
+            "b": "bh", 
+            "c": "ch", 
+            "d": "dh", 
+            "f": "fh", 
+            "g": "gh", 
+            "m": "mh", 
+            "p": "ph", 
+            "s": "ts", 
+            "t": "th"
+        };
 
+        let initial = noun.charAt(0).toLowerCase();
+        return Object.keys(lenitableInitials).includes(initial) ? lenitableInitials[initial] + noun.slice(1) : noun;
     }
 
-    mutate = (noun) => {
+    eclipse = (noun) => {
+        const eclipsableInitials = {
+            "a": "n-a",
+            "b": "mb",
+            "c": "gc",
+            "d": "nd",
+            "e": "n-e",
+            "f": "bhf",
+            "g": "ng",
+            "i": "n-i",
+            "o": "n-o",
+            "p": "bp",
+            "t": "dt",
+            "u": "n-u"
+        };
 
+        let initial = noun.charAt(0).toLowerCase();
+        return Object.keys(eclipsableInitials).includes(initial) ? eclipsableInitials[initial] + noun.slice(1) : noun;
     }
 
-    declineDefiniteNoun = (mutation, func, count) => {
+    declineDefiniteNoun = (ga, func, count) => {
+        let showArticle = ga.term.split(" ").length === 1;
         if(func === "nominative" && count === "singular") {
-            return "an " + mutation.nominativeSingular;
+            return (showArticle ? "an " : "") + this.mutate(ga.mutations.nominativeSingular, ga.gender, func, count);
         } else if(func === "nominative" && count === "plural") {
-            return "na " + mutation.nominativePlural;
+            return (showArticle ? "na " : "") + this.mutate(ga.mutations.nominativePlural, ga.gender, func, count);
         } else if(func === "genitive" && count === "singular") {
-            return "an " + mutation.genitiveSingular;
+            return (showArticle ? "an " : "") + this.mutate(ga.mutations.genitiveSingular, ga.gender, func, count);
         } else if(func === "genitive" && count === "plural") {
-            return "na " + mutation.genitivePlural;
+            return (showArticle ? "na " : "") + this.mutate(ga.mutations.genitivePlural, ga.gender, func, count);
         }
     }
 
@@ -67,21 +116,21 @@ export default class TermCard extends Component {
                         <div className="margin-content">
                             <Grid container spacing={16}>
                                 <Grid item xs={6}>
-                                    <Typography style={{ fontSize: '16px' }} variant="h5">{this.declineDefiniteNoun(ga.mutations, "nominative", "singular")}</Typography>
+                                    <Typography style={{ fontSize: '16px' }} variant="h5">{this.declineDefiniteNoun(ga, "nominative", "singular")}</Typography>
                                     <Typography style={{ fontSize: '14px' }} variant="subtitle1" color="textSecondary">nominative singular</Typography>
                                 </Grid>
                                 <Grid item xs={6}>
-                                    <Typography style={{ fontSize: '16px' }} variant="h5">{this.declineDefiniteNoun(ga.mutations, "nominative", "plural")}</Typography>
+                                    <Typography style={{ fontSize: '16px' }} variant="h5">{this.declineDefiniteNoun(ga, "nominative", "plural")}</Typography>
                                     <Typography style={{ fontSize: '14px' }} variant="subtitle1" color="textSecondary">nominative plural</Typography>
                                 </Grid>
                             </Grid>
                             <Grid container spacing={16}>
                                 <Grid item xs={6}>                                    
-                                    <Typography style={{ fontSize: '16px' }} variant="h5">{this.declineDefiniteNoun(ga.mutations, "genitive", "singular")}</Typography>
+                                    <Typography style={{ fontSize: '16px' }} variant="h5">{this.declineDefiniteNoun(ga, "genitive", "singular")}</Typography>
                                     <Typography style={{ fontSize: '14px' }} variant="subtitle1" color="textSecondary">genitive singular</Typography>
                                 </Grid>
                                 <Grid item xs={6}>
-                                    <Typography style={{ fontSize: '16px' }} variant="h5">{this.declineDefiniteNoun(ga.mutations, "genitive", "plural")}</Typography>
+                                    <Typography style={{ fontSize: '16px' }} variant="h5">{this.declineDefiniteNoun(ga, "genitive", "plural")}</Typography>
                                     <Typography style={{ fontSize: '14px' }} variant="subtitle1" color="textSecondary">genitive plural</Typography>
                                 </Grid>
                             </Grid>
