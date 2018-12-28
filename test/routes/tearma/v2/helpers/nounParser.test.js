91784915a9bc18d108b8d901e42a61c14ba1701f
Edmond O' Flynn <oflynned@tcd.ie>
2018-12-28 00:56:33 +0000

    extended unit tests for domain parsing

--- test/routes/tearma/v2/helpers/nounParser.test.js
+++ test/routes/tearma/v2/helpers/nounParser.test.js
@@ -6,6 +6,44 @@ const fixtures = require("./nounParserExamples");
 const nounParser = require("../../../../../routes/tearma/v2/helpers/nounParser");
 
 describe("noun parsing", () => {
+	function dropDb () {
+		return Promise.all([db.get("nouns").drop()]);
+	}
+
+	function loadFixture (fixture, length = 1) {
+		return new Promise((resolve) => {
+			nounParser.parseNounsFromData(fixture)
+				.then((nouns) => nouns.sort((a, b) => a.ga.term - b.ga.term))
+				.then((nouns) => {
+					expect(Array.isArray(nouns)).toBe(true);
+					expect(nouns.length).toEqual(length);
+					resolve(nouns.length === 1 ? nouns[0] : nouns);
+				});
+		});
+	}
+
+	describe("#loadFixture", () => {
+		test("should return object given single definition fixture length", (done) => {
+			loadFixture(fixtures.singleItemDefinition)
+				.then((result) => {
+					expect(typeof result).toBe("object");
+					expect(Array.isArray(result)).toEqual(false);
+					done();
+				});
+		});
+
+		test("should return array given multiple definition fixture length", (done) => {
+			loadFixture(fixtures.multipleItemDomain, 2)
+				.then((result) => {
+					expect(Array.isArray(result)).toEqual(true);
+					expect(result.length > 1).toBe(true);
+					expect(typeof result[0]).toBe("object");
+					expect(typeof result[1]).toBe("object");
+					done();
+				});
+		});
+	});
+
 	describe("#parseNounsFromData", () => {
 		beforeEach((done) => {
 			dropDb()
@@ -13,144 +51,132 @@ describe("noun parsing", () => {
 				.catch((err) => done(err));
 		});
 
-		function dropDb () {
-			return Promise.all([db.get("nouns").drop()]);
-		}
-
-		function loadFixture (fixture, length = 1) {
-			return new Promise((resolve) => {
-				nounParser.parseNounsFromData(fixture)
-					.then((nouns) => nouns.sort((a, b) => a.ga.term - b.ga.term))
-					.then((nouns) => {
-						expect(Array.isArray(nouns)).toBe(true);
-						expect(nouns.length).toBe(length);
-						resolve(nouns);
+		describe("domain parsing", () => {
+			test("with one domain", (done) => {
+				loadFixture(fixtures.singleDomain)
+					.then((noun) => {
+						expect(noun.en.domains).toEqual(["Law › Property Law"]);
+						expect(noun.ga.domains).toEqual(["Dlí › Dlí Réadmhaoine"]);
+						done();
 					});
 			});
-		}
 
-		test("with separate plurals", (done) => {
-			loadFixture(fixtures.separatePlurals)
-				.then((nouns) => {
-					expect(typeof nouns[0]).toBe("object");
-					expect(nouns[0].en.term).toBe("adductor muscle");
-					expect(nouns[0].ga.term).toBe("matán aduchtach");
-					expect(nouns[0].ga.mutations.nominativeSingular).toBe("matán aduchtach");
-					expect(nouns[0].ga.mutations.genitiveSingular).toBe("matáin aduchtaigh");
-					expect(nouns[0].ga.mutations.nominativePlural).toBe("matáin aduchtacha");
-					expect(nouns[0].ga.mutations.genitivePlural).toBe("matán aduchtach");
-					expect(nouns[0].ga.declension).toBe(1);
-					expect(nouns[0].ga.gender).toBe("masculine");
-					done();
-				})
-				.catch((err) => done(err));
+			test("with multiple domains", (done) => {
+				loadFixture(fixtures.multipleDomains)
+					.then((noun) => {
+						expect(noun.en.domains).toEqual(["Biology", "Medicine, Medical › Physiology"]);
+						expect(noun.ga.domains).toEqual(["Bitheolaíocht", "Leigheas › Fiseolaíocht"]);
+						done();
+					});
+			});
 		});
 
-		test("with combined plurals", (done) => {
-			loadFixture(fixtures.combinedPlural)
-				.then((nouns) => {
-					expect(typeof nouns[0]).toBe("object");
-					expect(nouns[0].en.term).toBe("occupier");
-					expect(nouns[0].ga.term).toBe("áititheoir");
-					expect(nouns[0].ga.mutations.nominativeSingular).toBe("áititheoir");
-					expect(nouns[0].ga.mutations.genitiveSingular).toBe("áititheora");
-					expect(nouns[0].ga.mutations.nominativePlural).toBe("áititheoirí");
-					expect(nouns[0].ga.mutations.genitivePlural).toBe("áititheoirí");
-					expect(nouns[0].ga.declension).toBe(3);
-					expect(nouns[0].ga.gender).toBe("masculine");
-					done();
-				})
-				.catch((err) => done(err));
-		});
+		describe("mutation parsing", () => {
+			test("with separate plurals", (done) => {
+				loadFixture(fixtures.separatePlurals)
+					.then((noun) => {
+						expect(noun.en.term).toEqual("adductor muscle");
+						expect(noun.ga.term).toEqual("matán aduchtach");
+						expect(noun.ga.mutations.nominativeSingular).toEqual("matán aduchtach");
+						expect(noun.ga.mutations.genitiveSingular).toEqual("matáin aduchtaigh");
+						expect(noun.ga.mutations.nominativePlural).toEqual("matáin aduchtacha");
+						expect(noun.ga.mutations.genitivePlural).toEqual("matán aduchtach");
+						expect(noun.ga.declension).toEqual(1);
+						expect(noun.ga.gender).toEqual("masculine");
+						done();
+					});
+			});
 
-		test("with only nominative singular", (done) => {
-			loadFixture(fixtures.onlyNominativeSingularForm)
-				.then((nouns) => {
-					expect(typeof nouns[0]).toBe("object");
-					expect(nouns[0].en.term).toBe("contingent charge");
-					expect(nouns[0].ga.term).toBe("muirear teagmhasach");
-					expect(nouns[0].ga.mutations.nominativeSingular).toBe("muirear teagmhasach");
-					expect(nouns[0].ga.mutations.genitiveSingular).toBeNull();
-					expect(nouns[0].ga.mutations.nominativePlural).toBeNull();
-					expect(nouns[0].ga.mutations.genitivePlural).toBeNull();
-					expect(nouns[0].ga.declension).toBe(1);
-					expect(nouns[0].ga.gender).toBe("masculine");
-					done();
-				})
-				.catch((err) => done(err));
-		});
+			test("with combined plurals", (done) => {
+				loadFixture(fixtures.combinedPlural)
+					.then((noun) => {
+						expect(noun.en.term).toEqual("occupier");
+						expect(noun.ga.term).toEqual("áititheoir");
+						expect(noun.ga.mutations.nominativeSingular).toEqual("áititheoir");
+						expect(noun.ga.mutations.genitiveSingular).toEqual("áititheora");
+						expect(noun.ga.mutations.nominativePlural).toEqual("áititheoirí");
+						expect(noun.ga.mutations.genitivePlural).toEqual("áititheoirí");
+						expect(noun.ga.declension).toEqual(3);
+						expect(noun.ga.gender).toEqual("masculine");
+						done();
+					});
+			});
 
-		test("with no declension", (done) => {
-			loadFixture(fixtures.noDeclension)
-				.then((nouns) => {
-					expect(typeof nouns[0]).toBe("object");
-					expect(nouns[0].en.term).toBe("customs");
-					expect(nouns[0].ga.term).toBe("custaim");
-					expect(nouns[0].ga.mutations.nominativeSingular).toBe("custaim");
-					expect(nouns[0].ga.mutations.genitiveSingular).toBeNull();
-					expect(nouns[0].ga.mutations.nominativePlural).toBeNull();
-					expect(nouns[0].ga.mutations.genitivePlural).toBe("custam");
-					expect(nouns[0].ga.declension).toBe(-1);
-					expect(nouns[0].ga.gender).toBe("masculine");
-					done();
-				})
-				.catch((err) => done(err));
-		});
+			test("with only nominative singular", (done) => {
+				loadFixture(fixtures.onlyNominativeSingularForm)
+					.then((noun) => {
+						expect(noun.en.term).toEqual("contingent charge");
+						expect(noun.ga.term).toEqual("muirear teagmhasach");
+						expect(noun.ga.mutations.nominativeSingular).toEqual("muirear teagmhasach");
+						expect(noun.ga.mutations.genitiveSingular).toBeNull();
+						expect(noun.ga.mutations.nominativePlural).toBeNull();
+						expect(noun.ga.mutations.genitivePlural).toBeNull();
+						expect(noun.ga.declension).toEqual(1);
+						expect(noun.ga.gender).toEqual("masculine");
+						done();
+					});
+			});
 
-		test("with multiple English terms to one Irish term", (done) => {
-			loadFixture(fixtures.multipleEnglishToOneIrish, 2)
-				.then((nouns) => {
-					expect(typeof nouns[0]).toBe("object");
-					expect(nouns[0].en.term).toBe("heir-at-law");
-					expect(nouns[0].ga.term).toBe("oidhre ginearálta");
-					expect(nouns[0].ga.mutations.nominativeSingular).toBe("oidhre ginearálta");
-					expect(nouns[0].ga.mutations.genitiveSingular).toBeNull();
-					expect(nouns[0].ga.mutations.nominativePlural).toBeNull();
-					expect(nouns[0].ga.mutations.genitivePlural).toBeNull();
-					expect(nouns[0].ga.declension).toBe(4);
-					expect(nouns[0].ga.gender).toBe("masculine");
+			test("with no declension", (done) => {
+				loadFixture(fixtures.noDeclension)
+					.then((noun) => {
+						expect(noun.en.term).toEqual("customs");
+						expect(noun.ga.term).toEqual("custaim");
+						expect(noun.ga.mutations.nominativeSingular).toEqual("custaim");
+						expect(noun.ga.mutations.genitiveSingular).toBeNull();
+						expect(noun.ga.mutations.nominativePlural).toBeNull();
+						expect(noun.ga.mutations.genitivePlural).toEqual("custam");
+						expect(noun.ga.declension).toEqual(-1);
+						expect(noun.ga.gender).toEqual("masculine");
+						done();
+					});
+			});
 
-					expect(typeof nouns[1]).toBe("object");
-					expect(nouns[1].en.term).toBe("heir at law");
-					expect(nouns[1].ga.term).toBe("oidhre ginearálta");
-					expect(nouns[1].ga.mutations.nominativeSingular).toBe("oidhre ginearálta");
-					expect(nouns[1].ga.mutations.genitiveSingular).toBeNull();
-					expect(nouns[1].ga.mutations.nominativePlural).toBeNull();
-					expect(nouns[1].ga.mutations.genitivePlural).toBeNull();
-					expect(nouns[1].ga.declension).toBe(4);
-					expect(nouns[1].ga.gender).toBe("masculine");
+			test("with multiple English terms to one Irish term", () => {
+				loadFixture(fixtures.multipleEnglishToOneIrish, 2)
+					.then((nouns) => {
+						expect(nouns[0].en.term).toEqual("heir-at-law");
+						expect(nouns[0].ga.term).toEqual("oidhre ginearálta");
+						expect(nouns[0].ga.mutations.nominativeSingular).toEqual("oidhre ginearálta");
+						expect(nouns[0].ga.mutations.genitiveSingular).toBeNull();
+						expect(nouns[0].ga.mutations.nominativePlural).toBeNull();
+						expect(nouns[0].ga.mutations.genitivePlural).toBeNull();
+						expect(nouns[0].ga.declension).toEqual(4);
+						expect(nouns[0].ga.gender).toEqual("masculine");
 
-					done();
-				})
-				.catch((err) => done(err));
-		});
+						expect(nouns[1].en.term).toEqual("heir at law");
+						expect(nouns[1].ga.term).toEqual("oidhre ginearálta");
+						expect(nouns[1].ga.mutations.nominativeSingular).toEqual("oidhre ginearálta");
+						expect(nouns[1].ga.mutations.genitiveSingular).toBeNull();
+						expect(nouns[1].ga.mutations.nominativePlural).toBeNull();
+						expect(nouns[1].ga.mutations.genitivePlural).toBeNull();
+						expect(nouns[1].ga.declension).toEqual(4);
+						expect(nouns[1].ga.gender).toEqual("masculine");
+					});
+			});
 
-		test("with multiple Irish terms to one English term", (done) => {
-			loadFixture(fixtures.multipleIrishToOneEnglish, 2)
-				.then((nouns) => {
-					expect(typeof nouns[0]).toBe("object");
-					expect(nouns[0].en.term).toBe("customs clearance");
-					expect(nouns[0].ga.term).toBe("imréiteach custam");
-					expect(nouns[0].ga.mutations.nominativeSingular).toBe("imréiteach custam");
-					expect(nouns[0].ga.mutations.genitiveSingular).toBeNull();
-					expect(nouns[0].ga.mutations.nominativePlural).toBeNull();
-					expect(nouns[0].ga.mutations.genitivePlural).toBeNull();
-					expect(nouns[0].ga.declension).toBe(1);
-					expect(nouns[0].ga.gender).toBe("masculine");
+			test("with multiple Irish terms to one English term", () => {
+				loadFixture(fixtures.multipleIrishToOneEnglish, 2)
+					.then((nouns) => {
+						expect(nouns[0].en.term).toEqual("customs clearance");
+						expect(nouns[0].ga.term).toEqual("imréiteach custam");
+						expect(nouns[0].ga.mutations.nominativeSingular).toEqual("imréiteach custam");
+						expect(nouns[0].ga.mutations.genitiveSingular).toBeNull();
+						expect(nouns[0].ga.mutations.nominativePlural).toBeNull();
+						expect(nouns[0].ga.mutations.genitivePlural).toBeNull();
+						expect(nouns[0].ga.declension).toEqual(1);
+						expect(nouns[0].ga.gender).toEqual("masculine");
 
-					expect(typeof nouns[1]).toBe("object");
-					expect(nouns[1].en.term).toBe("customs clearance");
-					expect(nouns[1].ga.term).toBe("imréiteach custaim");
-					expect(nouns[1].ga.mutations.nominativeSingular).toBe("imréiteach custaim");
-					expect(nouns[1].ga.mutations.genitiveSingular).toBeNull();
-					expect(nouns[1].ga.mutations.nominativePlural).toBeNull();
-					expect(nouns[1].ga.mutations.genitivePlural).toBeNull();
-					expect(nouns[1].ga.declension).toBe(1);
-					expect(nouns[1].ga.gender).toBe("masculine");
-
-					done();
-				})
-				.catch((err) => done(err));
+						expect(nouns[1].en.term).toEqual("customs clearance");
+						expect(nouns[1].ga.term).toEqual("imréiteach custaim");
+						expect(nouns[1].ga.mutations.nominativeSingular).toEqual("imréiteach custaim");
+						expect(nouns[1].ga.mutations.genitiveSingular).toBeNull();
+						expect(nouns[1].ga.mutations.nominativePlural).toBeNull();
+						expect(nouns[1].ga.mutations.genitivePlural).toBeNull();
+						expect(nouns[1].ga.declension).toEqual(1);
+						expect(nouns[1].ga.gender).toEqual("masculine");
+					});
+			});
 		});
 	});
 });
