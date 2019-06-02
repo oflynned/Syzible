0c7dc69e9e3daefe2f0fca91acaa6fadfae91184
Edmond O' Flynn <oflynned@tcd.ie>
2019-06-02 13:13:36 +0200

    refactor xml parser

--- routes/irishRail/v1/index.js
+++ routes/irishRail/v1/index.js
@@ -1,20 +1,18 @@
-import { ALL_STATIONS_ENDPOINT, getResource, STATION_DATA_ENDPOINT } from "./helpers/http";
-import { mapStations, mapStationTimes, parseXmlToJson } from "./helpers/xmlParser";
+import { getResource, ALL_STATIONS_ENDPOINT, STATION_DATA_ENDPOINT } from "./helpers/http";
+import { mapStations, mapStationTimes } from "./helpers/xmlParser";
 
 const express = require("express");
 let router = express.Router();
 
 router.get("/stations", async (req, res) => {
 	const xmlResponse = await getResource(ALL_STATIONS_ENDPOINT);
-	const jsonResponse = await parseXmlToJson(xmlResponse);
-	const mappedStations = await mapStations(jsonResponse);
+	const mappedStations = await mapStations(xmlResponse);
 	res.status(200).json(mappedStations);
 });
 
 router.get("/stations/:stationCode", async (req, res) => {
 	const xmlResponse = await getResource(STATION_DATA_ENDPOINT + req.params.stationCode);
-	const jsonResponse = await parseXmlToJson(xmlResponse);
-	const mappedStations = await mapStationTimes(jsonResponse);
+	const mappedStations = await mapStationTimes(xmlResponse);
 	res.status(200).json(mappedStations);
 });
 
