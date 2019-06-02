59a96c94a897ee610a374f9d88f974891ea539a6
Edmond O' Flynn <oflynned@tcd.ie>
2019-06-02 13:07:40 +0200

    create test routes

---
+++ test/routes/irishRail/v1/helpers/trainTestCases.js
@@ -0,0 +1,38 @@
+export const NO_STATIONS_XML =
+	"<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
+	"<ArrayOfObjStation xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns=\"http://api.irishrail.ie/realtime/\">\n" +
+	"</ArrayOfObjStation>";
+
+export const ONE_STATION_XML =
+	"<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
+	"<ArrayOfObjStation xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns=\"http://api.irishrail.ie/realtime/\">\n" +
+	"  <objStation>\n" +
+	"    <StationDesc>Belfast</StationDesc>\n" +
+	"    <StationAlias />\n" +
+	"    <StationLatitude>54.6123</StationLatitude>\n" +
+	"    <StationLongitude>-5.91744</StationLongitude>\n" +
+	"    <StationCode>BFSTC</StationCode>\n" +
+	"    <StationId>228</StationId>\n" +
+	"  </objStation>\n" +
+	"</ArrayOfObjStation>";
+
+export const TWO_STATIONS_XML =
+	"<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
+	"<ArrayOfObjStation xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns=\"http://api.irishrail.ie/realtime/\">\n" +
+	"  <objStation>\n" +
+	"    <StationDesc>Belfast</StationDesc>\n" +
+	"    <StationAlias />\n" +
+	"    <StationLatitude>54.6123</StationLatitude>\n" +
+	"    <StationLongitude>-5.91744</StationLongitude>\n" +
+	"    <StationCode>BFSTC</StationCode>\n" +
+	"    <StationId>228</StationId>\n" +
+	"  </objStation>\n" +
+	"  <objStation>\n" +
+	"    <StationDesc>Lisburn</StationDesc>\n" +
+	"    <StationAlias />\n" +
+	"    <StationLatitude>54.514</StationLatitude>\n" +
+	"    <StationLongitude>-6.04327</StationLongitude>\n" +
+	"    <StationCode>LBURN</StationCode>\n" +
+	"    <StationId>238</StationId>\n" +
+	"  </objStation>" +
+	"</ArrayOfObjStation>";
