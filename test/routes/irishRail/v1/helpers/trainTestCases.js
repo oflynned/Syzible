7a61041782ec435afab159a9ff1c7d38e6f0e0c1
Edmond O' Flynn <oflynned@tcd.ie>
2019-06-02 13:13:55 +0200

    add train test cases js

--- test/routes/irishRail/v1/helpers/trainTestCases.js
+++ test/routes/irishRail/v1/helpers/trainTestCases.js
@@ -1,38 +1,83 @@
-export const NO_STATIONS_XML =
+export const NO_TRAINS_XML =
 	"<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
-	"<ArrayOfObjStation xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns=\"http://api.irishrail.ie/realtime/\">\n" +
-	"</ArrayOfObjStation>";
+	"<ArrayOfObjStationData xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns=\"http://api.irishrail.ie/realtime/\">\n" +
+	"</ArrayOfObjStationData>";
 
-export const ONE_STATION_XML =
+export const ONE_TRAIN_XML =
 	"<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
-	"<ArrayOfObjStation xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns=\"http://api.irishrail.ie/realtime/\">\n" +
-	"  <objStation>\n" +
-	"    <StationDesc>Belfast</StationDesc>\n" +
-	"    <StationAlias />\n" +
-	"    <StationLatitude>54.6123</StationLatitude>\n" +
-	"    <StationLongitude>-5.91744</StationLongitude>\n" +
-	"    <StationCode>BFSTC</StationCode>\n" +
-	"    <StationId>228</StationId>\n" +
-	"  </objStation>\n" +
-	"</ArrayOfObjStation>";
+	"<ArrayOfObjStationData xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns=\"http://api.irishrail.ie/realtime/\">\n" +
+	"  <objStationData>\n" +
+	"    <Servertime>2019-06-02T11:59:44.497</Servertime>\n" +
+	"    <Traincode>A215 </Traincode>\n" +
+	"    <Stationfullname>Charleville</Stationfullname>\n" +
+	"    <Stationcode>CVILL</Stationcode>\n" +
+	"    <Querytime>11:59:44</Querytime>\n" +
+	"    <Traindate>02 Jun 2019</Traindate>\n" +
+	"    <Origin>Cork</Origin>\n" +
+	"    <Destination>Dublin Heuston</Destination>\n" +
+	"    <Origintime>12:25</Origintime>\n" +
+	"    <Destinationtime>14:55</Destinationtime>\n" +
+	"    <Status>No Information</Status>\n" +
+	"    <Lastlocation />\n" +
+	"    <Duein>66</Duein>\n" +
+	"    <Late>0</Late>\n" +
+	"    <Exparrival>13:04</Exparrival>\n" +
+	"    <Expdepart>13:05</Expdepart>\n" +
+	"    <Scharrival>13:04</Scharrival>\n" +
+	"    <Schdepart>13:05</Schdepart>\n" +
+	"    <Direction>To Dublin Heuston</Direction>\n" +
+	"    <Traintype>MK4</Traintype>\n" +
+	"    <Locationtype>S</Locationtype>\n" +
+	"  </objStationData>\n" +
+	"</ArrayOfObjStationData>";
 
-export const TWO_STATIONS_XML =
+export const TWO_TRAINS_XML =
 	"<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
-	"<ArrayOfObjStation xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns=\"http://api.irishrail.ie/realtime/\">\n" +
-	"  <objStation>\n" +
-	"    <StationDesc>Belfast</StationDesc>\n" +
-	"    <StationAlias />\n" +
-	"    <StationLatitude>54.6123</StationLatitude>\n" +
-	"    <StationLongitude>-5.91744</StationLongitude>\n" +
-	"    <StationCode>BFSTC</StationCode>\n" +
-	"    <StationId>228</StationId>\n" +
-	"  </objStation>\n" +
-	"  <objStation>\n" +
-	"    <StationDesc>Lisburn</StationDesc>\n" +
-	"    <StationAlias />\n" +
-	"    <StationLatitude>54.514</StationLatitude>\n" +
-	"    <StationLongitude>-6.04327</StationLongitude>\n" +
-	"    <StationCode>LBURN</StationCode>\n" +
-	"    <StationId>238</StationId>\n" +
-	"  </objStation>" +
-	"</ArrayOfObjStation>";
+	"<ArrayOfObjStationData xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns=\"http://api.irishrail.ie/realtime/\">\n" +
+	"  <objStationData>\n" +
+	"    <Servertime>2019-06-02T11:57:12.1</Servertime>\n" +
+	"    <Traincode>A208 </Traincode>\n" +
+	"    <Stationfullname>Mallow</Stationfullname>\n" +
+	"    <Stationcode>MLLOW</Stationcode>\n" +
+	"    <Querytime>11:57:12</Querytime>\n" +
+	"    <Traindate>02 Jun 2019</Traindate>\n" +
+	"    <Origin>Dublin Heuston</Origin>\n" +
+	"    <Destination>Cork</Destination>\n" +
+	"    <Origintime>10:00</Origintime>\n" +
+	"    <Destinationtime>12:30</Destinationtime>\n" +
+	"    <Status>En Route</Status>\n" +
+	"    <Lastlocation>Departed Charleville</Lastlocation>\n" +
+	"    <Duein>14</Duein>\n" +
+	"    <Late>4</Late>\n" +
+	"    <Exparrival>12:09</Exparrival>\n" +
+	"    <Expdepart>12:11</Expdepart>\n" +
+	"    <Scharrival>12:05</Scharrival>\n" +
+	"    <Schdepart>12:07</Schdepart>\n" +
+	"    <Direction>To Cork</Direction>\n" +
+	"    <Traintype>MK4</Traintype>\n" +
+	"    <Locationtype>S</Locationtype>\n" +
+	"  </objStationData>\n" +
+	"  <objStationData>\n" +
+	"    <Servertime>2019-06-02T11:57:12.1</Servertime>\n" +
+	"    <Traincode>D266 </Traincode>\n" +
+	"    <Stationfullname>Mallow</Stationfullname>\n" +
+	"    <Stationcode>MLLOW</Stationcode>\n" +
+	"    <Querytime>11:57:12</Querytime>\n" +
+	"    <Traindate>02 Jun 2019</Traindate>\n" +
+	"    <Origin>Mallow</Origin>\n" +
+	"    <Destination>Cork</Destination>\n" +
+	"    <Origintime>13:25</Origintime>\n" +
+	"    <Destinationtime>13:52</Destinationtime>\n" +
+	"    <Status>No Information</Status>\n" +
+	"    <Lastlocation />\n" +
+	"    <Duein>88</Duein>\n" +
+	"    <Late>0</Late>\n" +
+	"    <Exparrival>00:00</Exparrival>\n" +
+	"    <Expdepart>13:25</Expdepart>\n" +
+	"    <Scharrival>00:00</Scharrival>\n" +
+	"    <Schdepart>13:25</Schdepart>\n" +
+	"    <Direction>To Cork</Direction>\n" +
+	"    <Traintype>ARROW</Traintype>\n" +
+	"    <Locationtype>O</Locationtype>\n" +
+	"  </objStationData>\n" +
+	"</ArrayOfObjStationData>";
